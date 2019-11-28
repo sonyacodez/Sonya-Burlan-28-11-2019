6cf3e7df862767c3c5d77471a5dfe03264c1fabb
Sonya Burlan <sonya.burlan@gmail.com>
2019-11-28 14:46:24 +0200

    created the basic first 3 components to go into App.js

---
+++ src/components/AllFavorites.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as SingleFavorite from './SingleFavorite';
+
+export const AllFavorites = () => (
+    <SingleFavorite/>
+);
---
+++ src/components/SingleFavorite.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SingleFavorite = () => (
+    <div>Single Favorite Component</div>
+);

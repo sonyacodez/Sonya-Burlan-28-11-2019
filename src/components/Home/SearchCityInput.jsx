d01c9a74eec68a5bcf04191ac0c6b5a4ad7a5233
Sonya Burlan <sonya.burlan@gmail.com>
2019-11-28 17:19:43 +0200

    wrote basic SearchCityInput component

---
+++ src/components/Home/SearchCityInput.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Input, Button } from '@material-ui/core';
+
+export const SearchCityInput = () => (
+    <div>
+        <Input style={{ marginTop: "50px" }} />
+        <Button>Search</Button>
+    </div>
+);

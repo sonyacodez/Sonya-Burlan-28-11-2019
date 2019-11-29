90354a8263512588e4e55bfe79f10ab23e482adc
Sonya Burlan <sonya.burlan@gmail.com>
2019-11-29 16:35:35 +0200

    added the SearchCityInput component

--- src/components/Home/SearchCityInput.jsx
+++ src/components/Home/SearchCityInput.jsx
@@ -1,9 +1,28 @@
-import React from "react";
-import { Input, Button } from '@material-ui/core';
+import React, { useContext, useState } from "react";
+import apiClient from '../../apiClient';
+import { observer } from "mobx-react-lite";
+import TextField from '@material-ui/core/TextField';
+import CityStoreContext from '../../stores/CityStore';
+import Autocomplete from '@material-ui/lab/Autocomplete';
 
-export const SearchCityInput = () => (
-    <div>
-        <Input style={{ marginTop: "50px" }} />
-        <Button>Search</Button>
-    </div>
-);
+export const SearchCityInput = observer(() => {
+    const cityStore = useContext(CityStoreContext);
+    const [ citySuggestions, setCitySuggestions ] = useState([]);
+    return (
+        <Autocomplete
+            autoComplete={true}
+            disableOpenOnFocus={true}
+            options={citySuggestions}
+            getOptionLabel={option => option}
+            onInputChange={async(e) => {
+                if(e){
+                    setCitySuggestions(await apiClient.searchCityAutoCompleteInput(e.target.value))
+                }else{
+                    setCitySuggestions([])
+                }
+            }}
+            onChange={(e, value) => cityStore.currentCity = value}
+            renderInput={params => <TextField {...params} variant="outlined" fullWidth/>}
+        />
+    );
+});

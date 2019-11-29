ea0d7f14cebf92a7570b6176e7abde8d139891dc
Sonya Burlan <sonya.burlan@gmail.com>
2019-11-29 19:36:53 +0200

    added HomeCard with MobX observer with Grid and Card Material UI

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { SearchCityInput } from './SearchCityInput';
 import { HomeCard } from './HomeCard';
 
--- src/components/Home/HomeCard.jsx
+++ src/components/Home/HomeCard.jsx
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { observer } from "mobx-react-lite";
+import CityStoreContext from '../../stores/CityStore';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Card, Button, CardContent, CardActions, Typography } from '@material-ui/core';
 
-export const HomeCard = () => (
-    <h1>
-        <strong>
-            HomeCard Component
-        </strong>
-    </h1>
-)
+export const HomeCard = observer(() => {
+    const cityStore = useContext(CityStoreContext);
+    const classes = useStyles();
+    return (
+        <Grid container 
+                alignItems="center" 
+                justify="center" 
+                className={classes.root} 
+                spacing={0}>
+            <Grid item xs={12}>
+                <Card className={classes.card}>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">
+                            {cityStore.currentCity}
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+            </Grid>
+        </Grid>
+    );
+});
+
+const useStyles = makeStyles(() => ({
+    root: {
+        flexGrow: 1
+    },
+    card: {
+        height: 300,
+        width: 500,
+        marginLeft: "33vw",
+        marginTop: "15vh",
+    }
+}));
+  

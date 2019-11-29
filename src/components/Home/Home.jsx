38e90d264c9bc01b1ea3582f9d8f574cecd4ed81
Sonya Burlan <sonya.burlan@gmail.com>
2019-11-29 16:36:54 +0200

    added HomeCard to Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { SearchCityInput } from './SearchCityInput';
+import { HomeCard } from './HomeCard';
 
 export const Home = () => (
     <div>
         <SearchCityInput/>
+        <HomeCard/>
     </div>
 );

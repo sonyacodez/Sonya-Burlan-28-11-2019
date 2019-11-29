790d1a02460b4529727486bd3b390811d25bd3ca
Sonya Burlan <sonya.burlan@gmail.com>
2019-11-29 19:37:29 +0200

    added Material UI Grid and Paper to Favorites

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,6 +1,31 @@
 import React from "react";
-import { SingleFavorite } from './SingleFavorite';
+import { Grid, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+// import { SingleFavorite } from './SingleFavorite';
 
-export const Favorites = () => (
-    <SingleFavorite/>
-);
+export const Favorites = () => {
+    const classes = useStyles();
+    return (
+        <Grid container className={classes.root} spacing={2}>
+            <Grid item xs={12}>
+                <Grid container justify="center" spacing={2}>
+                    {[0, 1, 2].map(value => (
+                        <Grid key={value} item>
+                            <Paper className={classes.paper} />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Grid>
+        </Grid>
+    );
+};
+
+const useStyles = makeStyles(() => ({
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      height: 200,
+      width: 300
+    }
+}));

e98b05a3e34db7cfea0c626f4233cfac03677bee
Sonya Burlan <sonya.burlan@gmail.com>
2019-11-30 02:30:42 +0200

    added the ToastContainer from react-toastify to App component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import { Home } from './components/Home/Home';
-import { Favorites } from './components/Favorites/Favorites';
-import { BrowserRouter, Route, Link } from "react-router-dom"
+import React from "react";
+import { Home } from "./components/Home/Home";
+import { ToastContainer } from "react-toastify";
+import { Favorites } from "./components/Favorites/Favorites";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 export const App = () => (
-  <div className="App">
-      <BrowserRouter>
-        <header>
-          <Link to="/">Home</Link>
-          <Link to="/favorites">Favorites</Link>
-        </header>
-        <div>
-          <Route exact path="/" render={() => <Home/>}/>
-          <Route exact path="/favorites" render={() => <Favorites/>} />
-        </div>
-      </BrowserRouter>
-  </div>
+    <div className="App">
+        <BrowserRouter>
+            <header>
+                <Link to="/">Home</Link>
+                <Link to="/favorites">Favorites</Link>
+            </header>
+            <div>
+                <Route exact path="/" render={() => <Home />} />
+                <Route exact path="/favorites" render={() => <Favorites />} />
+            </div>
+            <ToastContainer />
+        </BrowserRouter>
+    </div>
 );

508730ae664ee0d02a7df2a6339c21df04d2773b
Sonya Burlan <sonya.burlan@gmail.com>
2019-11-30 12:33:03 +0200

    completedadded CurrentWeather component. Added useState and useEffect hooks

--- src/components/Home/HomeCard/CurrentWeather.jsx
+++ src/components/Home/HomeCard/CurrentWeather.jsx
@@ -1,18 +1,30 @@
-import React, { useContext } from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useContext, useState, useEffect } from 'react';
 import { Paper } from '@material-ui/core';
 import { observer } from "mobx-react-lite";
-import apiClient from '../../../ApiClient/apiClient';
 import { makeStyles } from '@material-ui/core/styles';
 import CityStoreContext from '../../../stores/CityStore';
+import toastApiClient from '../../../ApiClient/ToastApiClient';
 
-export const currentWeather = observer(() => {
-    const cityStore = useContext(CityStoreContext);
+export const CurrentWeather = observer(() => {
     const classes = useStyles();
+    const cityStore = useContext(CityStoreContext);
+    const [ cityWeather, setCityWeather ] = useState({});
+    useEffect(() => {
+        const fetchCityWeather = async() => {
+            setCityWeather(await toastApiClient.getCurrentCityWeather(cityStore.currentCity))
+        }
+        fetchCityWeather()
+    }, [])
     return (
-        <Paper className={classes.paper}>
-            {cityStore.currentCity}
-            {apiClient.getCurrentCityLocationKey()}
-        </Paper>
+        <div>
+            <Paper className={classes.paper}>
+                <div>{cityStore.currentCity}</div>
+                <div>{cityWeather.weatherDescription}</div>
+                <div>{cityWeather.celsius}</div>
+                <div>{cityWeather.fahrenheit}</div>
+            </Paper>
+        </div>
     );
 });
 

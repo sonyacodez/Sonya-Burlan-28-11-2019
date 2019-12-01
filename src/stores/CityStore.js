5fc8ee4d536035520d7d87542ce6b84c7b282595
Sonya Burlan <sonya.burlan@gmail.com>
2019-12-01 10:01:00 +0200

    added local storage functionality to CityStore and packe.json

--- src/stores/CityStore.js
+++ src/stores/CityStore.js
@@ -1,10 +1,26 @@
 import { observable } from 'mobx';
 import { createContext } from 'react';
+import store2 from 'store2';
 
 class CityStore {
     @observable currentCity = "Tel Aviv";
     @observable temperatureScale = "celsius";
-    @observable favoriteCities = [];
+    @observable favoriteCities = store2.get("favoriteCities", []);
+
+    updateFavoriteCities = (cities) => {
+        this.favoriteCities = cities;
+        store2.set("favoriteCities", cities);
+    };
+
+    addFavoriteCity = (city) => {
+        this.favoriteCities.push(city);
+        store2.set("favoriteCities", this.favoriteCities);
+    };
+
+    deleteFavoriteCity = (city) => {
+        this.favoriteCities = this.favoriteCities.filter(candidate => city !== candidate);
+        store2.set("favoriteCities", this.favoriteCities);
+    };
 };
 
 const CityStoreContext = createContext(new CityStore());

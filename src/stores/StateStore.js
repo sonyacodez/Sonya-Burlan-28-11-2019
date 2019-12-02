73e8f80f2ff314df71893eceef07c2a4d783877d
Sonya Burlan <sonya.burlan@gmail.com>
2019-12-02 21:26:46 +0200

    exported staticdata from stateStore

--- src/stores/StateStore.js
+++ src/stores/StateStore.js
@@ -1,21 +1,11 @@
 import { observable } from 'mobx';
 import { createContext } from 'react';
 import store2 from 'store2';
-import {
-    faSun,
-    faWind,
-    faBolt, 
-    faSnowflake,
-    faCloudSun, 
-    faCloudShowersHeavy
-} from '@fortawesome/free-solid-svg-icons';
 
-class StateStore {
+export class StateStore {
     @observable currentCity = "Tel Aviv";
     @observable temperatureScale = "celsius";
     @observable favoriteCities = store2.get("favoriteCities", []);
-
-    daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 
     updateFavoriteCities = cities => {
         this.favoriteCities = cities;
@@ -30,19 +20,6 @@ class StateStore {
     deleteFavoriteCity = city => {
         this.favoriteCities = this.favoriteCities.filter(candidate => city !== candidate);
         store2.set("favoriteCities", this.favoriteCities);
-    };
-    
-    whichDay = unformattedDate => this.daysOfWeek[new Date(unformattedDate).getDay()];
-
-    whichWeatherIcon = text => {
-        return text.toLowerCase().includes("cloud") ? faCloudSun :
-        text.toLowerCase().includes("rain") ? faCloudShowersHeavy :
-        text.toLowerCase().includes("showers") ? faCloudShowersHeavy :
-        text.toLowerCase().includes("thunder") ? faBolt :
-        text.toLowerCase().includes("snow") ? faSnowflake :
-        text.toLowerCase().includes("sun") ? faSun :
-        text.toLowerCase().includes("wind") ? faWind :
-        null;
     };
 };
 

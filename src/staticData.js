d6a797b9096198fa239b04966c21a4fd1f4b08ea
Sonya Burlan <sonya.burlan@gmail.com>
2019-12-02 21:25:08 +0200

    exported constant functions to staticData

---
+++ src/staticData.js
@@ -0,0 +1,40 @@
+
+import {
+    faSun,
+    faWind,
+    faBolt, 
+    faSnowflake,
+    faCloudSun, 
+    faCloudShowersHeavy
+} from '@fortawesome/free-solid-svg-icons';
+
+class StaticData {
+    constructor(){
+        this.daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    }
+
+    whichDay = unformattedDate => this.daysOfWeek[new Date(unformattedDate).getDay()]
+    
+    getDate = unformattedDate => {
+        const sanitizedDate = new Date(unformattedDate).toDateString();
+        const newDate = sanitizedDate.split(" ");
+        newDate.shift();
+        return newDate.join(" ");
+    }
+    
+    whichWeatherIcon = text => {
+        if(!text){
+            return faSun;
+        }
+        return text.toLowerCase().includes("cloud") ? faCloudSun :
+        text.toLowerCase().includes("rain") ? faCloudShowersHeavy :
+        text.toLowerCase().includes("showers") ? faCloudShowersHeavy :
+        text.toLowerCase().includes("thunder") ? faBolt :
+        text.toLowerCase().includes("snow") ? faSnowflake :
+        text.toLowerCase().includes("sun") ? faSun :
+        text.toLowerCase().includes("wind") ? faWind :
+        null;
+    }
+}
+
+export default new StaticData();

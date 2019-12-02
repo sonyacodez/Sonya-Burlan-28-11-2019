bf1dfb27e9f63d272bda2523024d24dfaa24effc
Sonya Burlan <sonya.burlan@gmail.com>
2019-12-02 02:15:47 +0200

    added useStyles, icon rendering based on weather description, and day instead of date in FiveDayForecast

--- src/components/Home/HomeCard/FiveDayForecast.jsx
+++ src/components/Home/HomeCard/FiveDayForecast.jsx
@@ -1,14 +1,34 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { observer } from "mobx-react-lite";
-import { makeStyles, Paper } from '@material-ui/core';
+import { makeStyles, Paper, Grid } from '@material-ui/core';
 import CityStoreContext from '../../../stores/CityStore';
 import toastApiClient from '../../../ApiClient/ToastApiClient';
+import {
+    faSun,
+    faWind,
+    faBolt, 
+    faSnowflake,
+    faCloudSun, 
+    faCloudShowersHeavy
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const FiveDayForecast = observer(() => {
     const classes = useStyles();
     const cityStore = useContext(CityStoreContext);
     const [ currentCity, setCurrentCity ] = useState(undefined);
     const [ fiveDayForecast, setFiveDayForecast ] = useState([]);
+    const daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    const whichDay = unformattedDate => daysOfWeek[new Date(unformattedDate).getDay()];
+    const weatherIcon = text => {
+        return text.toLowerCase().includes("cloud") ? faCloudSun :
+        text.toLowerCase().includes("rain") ? faCloudShowersHeavy :
+        text.toLowerCase().includes("thunder") ? faBolt :
+        text.toLowerCase().includes("snow") ? faSnowflake :
+        text.toLowerCase().includes("sun") ? faSun :
+        text.toLowerCase().includes("wind") ? faWind :
+        null;
+    };
     useEffect(() => {
         const fetchFiveDayForecast = async() => {
             setFiveDayForecast(
@@ -21,23 +41,47 @@ export const FiveDayForecast = observer(() => {
         }
     }, [currentCity, cityStore.currentCity, cityStore.temperatureScale]);
     return (
-        <div>
+        <Grid container spacing={1}>
             {fiveDayForecast.map((day, index) => (
-                <Paper key={index} className={classes.paper}>
-                    <div>{day.weatherDescription}</div>
-                    <div>{day.highTemperature}</div>
-                    <div>{day.lowTemperature}</div>
-                    <div>{day.date}</div>
-                    <div>{day.cityName}</div>
-                </Paper>
+                <Grid className={classes.root} item xs>
+                    <div className={classes.fiveDayForecast}>
+                        <Paper key={index} className={classes.paper}>
+                            <div>{whichDay(day.date)}</div>
+                            <FontAwesomeIcon size="5x" icon={weatherIcon(day.weatherDescription)}/>
+                            <div>
+                                <span className={classes.highTemperature}>{day.highTemperature}</span>
+                                <span className={classes.separator}> / </span>
+                                <span className={classes.lowTemperature}>{day.lowTemperature}</span>
+                            </div>
+                        </Paper>
+                    </div>
+                </Grid>
             ))}
-        </div>
+        </Grid>
     );
 });
 
 const useStyles = makeStyles(() => ({
+    root: {
+        display: "flex"
+    },
+    fiveDayForecast: {
+        padding: "1.3vmin",
+        flexDirection: "column",
+        justifyContent: "center"
+    },
     paper: {
-        height: 150,
-        width: 200
+        height: '22vh',
+        borderRadius: "40px",
+        fontSize: "2.3vh"
+    },
+    highTemperature: {
+        color: "red"
+    },
+    lowTemperature: {
+        color: "blue"
+    },
+    separator: {
+        color: "black"
     }
 }));

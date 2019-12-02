23c759535568496d29e203d134d43b627a188f4a
Sonya Burlan <sonya.burlan@gmail.com>
2019-12-02 21:24:50 +0200

    added GeoLocation bonus

--- src/ApiClient/apiClient.js
+++ src/ApiClient/apiClient.js
@@ -1,11 +1,10 @@
 import axios from 'axios'
-// import CityStoreContext from '../stores/CityStore';
 
 class ApiClient {
     constructor() {
-        this.urlBase = "https://dataservice.accuweather.com/"
-        this.myAPIkey = "1YfKkmecK5npET1GUyGVGIBr1vuYxx1c"; 
-        this.accessKey = "2c68386b32d6b46ea4dde87b3753597e29be3b00804ea5b7ed42b91dc9fb2dca";
+        this.urlBase = "https://dataservice.accuweather.com/";
+        this.myAPIkey = "t99CNXU2913kDvKpw1PoOQIZjG9O831r";
+        this.GeoAPIKey = "AIzaSyB7bTH7gOg_LUILwwIFYuXSiCwDR1beic8";
         /* I know this isn't good practice to commit the API key/accesskey, I'm keeping it here for now for simplicity's sake.*/
     }
 
@@ -14,7 +13,28 @@ class ApiClient {
         return matchingCities.data.map(city => city.LocalizedName);
     };
 
+    getDecodedAddress = async(lat, lng) => {
+        return await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${this.GeoAPIKey}`);
+    };
+
+    getUserCurrentAddressCity = () => {
+        return new Promise((resolve, reject)=>{
+            navigator.geolocation.getCurrentPosition(async (pos) => {
+                const address = await this.getDecodedAddress(pos.coords.latitude, pos.coords.longitude)
+                const city = address.data.results[0].formatted_address.split(",")[1].trim("");
+                console.log(city);
+                resolve(city);
+            },
+            (error)=>{
+                reject(error);
+            });
+        });
+    };
+
     getCurrentCityKey = async(currentCity) => {
+        if(!currentCity){
+            return "";
+        }
         const currentCityInfo = await axios.get(`${this.urlBase}locations/v1/cities/autocomplete?apikey=${this.myAPIkey}&q=${currentCity}`);
         return currentCityInfo.data[0].Key;
     };
@@ -27,6 +47,7 @@ class ApiClient {
             weatherDescription: currentCityWeather.WeatherText,
             celsius: currentCityWeather.Temperature.Metric.Value,
             fahrenheit: currentCityWeather.Temperature.Imperial.Value,
+            dateAndTime: currentCityWeather.LocalObservationDateTime
         };
     };
 
@@ -34,50 +55,19 @@ class ApiClient {
         const cityKey = await this.getCurrentCityKey(currentCity);
         const temperatureScale = temperatureScalePreference === "celsius" ? true : false;
         const fiveDayForecast = await axios.get(`${this.urlBase}forecasts/v1/daily/5day/${cityKey}?apikey=${this.myAPIkey}&details=false&metric=${temperatureScale}`);
-        return fiveDayForecast.data.DailyForecasts.map(day => {
-            return {
-                weatherDescription: day.Day.IconPhrase,
-                highTemperature: day.Temperature.Maximum.Value,
-                lowTemperature: day.Temperature.Minimum.Value,
-                date: day.Date,
-                cityName: currentCity
-            }
-        });
+        return fiveDayForecast.data.DailyForecasts.map(day => ({
+            weatherDescription: day.Day.IconPhrase,
+            highTemperature: day.Temperature.Maximum.Value,
+            lowTemperature: day.Temperature.Minimum.Value,
+            date: day.Date,
+            cityName: currentCity
+        }));
     };
 
     getCurrentCityWeatherPhoto = async() => {
-        const cityPhoto = await axios.get(`https://source.unsplash.com/1920x1080/?beautiful+nature+sunrises+sunsets+green+light`);
+        const cityPhoto = await axios.get(`https://source.unsplash.com/1920x1080/?+nature+europe+countryside+water`);
         return cityPhoto.config.url
     };
-
-
-    // getLatLongOfAddress = async(address) => {
-    //     return await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${this.key}`)
-    // }
-
-    // addNewUser = async( name, email, latitude, longitude, address, subscriptionObject) => {
-    //     const newUser = {
-    //         name,
-    //         email,
-    //         location: {
-    //             latitude,
-    //             longitude,
-    //             address
-    //         },
-    //         subscriptionObject: JSON.parse(subscriptionObject)
-    //     }
-    //     await axios.post(`${this.URLname}/subscribe`, newUser)
-    // };
-
-    // updateUserLocation = async(latitude, longitude, address) => await axios.put(`${this.URLname}/updateUserLocation/${UserStore.currentUserID}`, { location: {latitude, longitude, address}});
-
-    // getAllContacts = async () => await axios.get(`${this.URLname}/userContacts/${UserStore.currentUserID}`);
-
-    // deleteUserContact = async(userID, contactID) => await axios.delete(`${this.URLname}/deleteUserContact/${contactID}`);
-
-    // addUserContact = async (name, phoneNumber) => await axios.post(`${this.URLname}/newUserContact/${UserStore.currentUserID}`, { name, phoneNumber });
-
-    // updateUserContactNumber = async (contactID, phoneNumber) => await axios.put(`${this.URLname}/updateUserContactNumber/${contactID}`, { phoneNumber });
 }
 
 export default new ApiClient()
--- src/index.js
+++ src/index.js
@@ -3,7 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
+import apiClient from './ApiClient/apiClient';
+import StateStoreContext, { StateStore } from './stores/StateStore';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+const stateStore = new StateStore();
+
+(async() => {
+    try {
+        stateStore.currentCity = await apiClient.getUserCurrentAddressCity();
+    }catch(error){
+        console.error(`Unable to fetch user GeoLocation.`);
+    }
+})();
+
+ReactDOM.render(<StateStoreContext.Provider value={stateStore}><App/></StateStoreContext.Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();

640f7eddb0b2210a039c57b5c1f8ea12fc67f46b
Sonya Burlan <sonya.burlan@gmail.com>
2019-12-04 05:20:00 +0200

    fixing heroku bug

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import StateStoreContext, { StateStore } from './stores/StateStore';
 
 const stateStore = new StateStore();
 
-// (async() => {
-//     try {
-//         stateStore.currentCity = await ApiClient.getUserCurrentAddressCity();
-//     }catch(error){
-//         console.error(`Unable to fetch user GeoLocation.`);
-//     }
-// })();
+(async() => {
+    try {
+        stateStore.currentCity = await ApiClient.getUserCurrentAddressCity();
+    }catch(error){
+        console.error(`Unable to fetch user GeoLocation.`);
+    }
+})();
 
 ReactDOM.render(<StateStoreContext.Provider value={stateStore}><App/></StateStoreContext.Provider>, document.getElementById('root'));
 

c2d2f879ae9ab0922a96b8be49aef49efd1dd487
Sonya Burlan <sonya.burlan@gmail.com>
2019-12-04 03:28:55 +0200

    deleted header tag encompassing Header component

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import React, { useEffect, useState, useContext } from "react";
 import { observer } from "mobx-react-lite";
 import apiClient from './apiclient/apiClient';
 import { Home } from "./components/Home/Home";
+import { makeStyles } from '@material-ui/core';
 import { ToastContainer } from "react-toastify";
 import StateStoreContext from './stores/StateStore';
-import { Favorites } from "./components/Favorites/Favorites";
-import { BrowserRouter, Route } from "react-router-dom";
 import { Header } from "./components/Header/Header";
-import { makeStyles } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import { BrowserRouter, Route } from "react-router-dom";
+import { createMuiTheme } from '@material-ui/core/styles';
+import { Favorites } from "./components/Favorites/Favorites";
 // import { ThemeProvider } from 'styled-components';
 // import { lightTheme, darkTheme } from './globalstyles/theme';
-import { GlobalStyles } from './globalstyles/global';
 import "./App.css";
 
-import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
-
-import { ThemeProvider } from '@material-ui/styles';
 const lightTheme = createMuiTheme();
+
 const darkTheme = createMuiTheme({palette: {type: "dark"}});
 
 export const App = observer(() => {
     const classes = useStyles();
     const stateStore = useContext(StateStoreContext);
+    const themeClassName = stateStore.currentTheme === 'light' ? 'lightTheme' : 'darkTheme';
     const [ imageUrl, setImageUrl ] = useState(async()=>{await apiClient.getCurrentCityWeatherPhoto()});
     useEffect(() => {
         const fetchBackgroundPhoto = async() => {
@@ -29,15 +29,11 @@ export const App = observer(() => {
         };
         fetchBackgroundPhoto();
     }, []);
-    const themeClassName = stateStore.currentTheme === 'light' ? 'lightTheme' : 'darkTheme';
     return (
         <ThemeProvider theme={stateStore.currentTheme === 'light' ? lightTheme : darkTheme}>
-            {GlobalStyles}
             <div id="App" className={`${classes.root} ${themeClassName}`} style={{ backgroundImage: `url(${imageUrl})` }}>
                 <BrowserRouter>
-                    <header>
-                        <Header/>
-                    </header>
+                    <Header/>
                     <div id="route" className={classes.route}>
                         <Route exact path="/" render={() => <Home />} />
                         <Route exact path="/favorites" render={() => <Favorites />} />
